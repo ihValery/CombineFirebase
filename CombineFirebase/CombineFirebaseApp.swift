177efa1c3778b8adfaf2536068b8f30793f8b32d
Valery <74760519+ihValery@users.noreply.github.com>
2021-05-29 20:36:51 +0300

    Initial Commit

---
+++ CombineFirebase/CombineFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFirebaseApp.swift
+//  CombineFirebase
+//
+//  Created by Валерий Игнатьев on 29.05.21.
+//
+
+import SwiftUI
+
+@main
+struct CombineFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
